e62a8c8034b6e7795015de4ef5ce7356919ab53f
moreal <dev.moreal@gmail.com>
2020-07-19 03:42:47 +0900

    feat(#2): use credentials from variables also

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 provider "google" {
-  credentials = file("credentials/account.json")
+  credentials = var.google_credential == "" ? file("credentials/account.json") : var.google_credential
   project     = var.project_id
 }
 
 provider "google-beta" {
-  credentials = file("credentials/account.json")
+  credentials = var.google_credential == "" ? file("credentials/account.json") : var.google_credential
   project     = var.project_id
 }
